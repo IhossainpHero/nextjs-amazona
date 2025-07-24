f73a1b979e348162f82c9a0ecf8365226e8b63df
IhossainpHero <imranmollah9131@gmai;.com>
2025-07-25 01:40:13 +0600

    third  commit

--- .next/server/chunks/ssr/[root-of-the-server]__9de99b35._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__9de99b35._.js
@@ -28,11 +28,11 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist
 const page = ()=>{
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
         className: "h1-bold text-center p-10",
-        children: "Website is developing"
+        children: "🛠️ The website is still under construction 🛠️ We’re currently working hard to bring you a better experience. Some features may not be available at the moment. Thank you for your patience and support. — Imran"
     }, void 0, false, {
         fileName: "[project]/app/(home)/page.tsx",
-        lineNumber: 2,
-        columnNumber: 10
+        lineNumber: 3,
+        columnNumber: 5
     }, ("TURBOPACK compile-time value", void 0));
 };
 const __TURBOPACK__default__export__ = page;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,11 @@
 const page = () => {
-  return <h1 className="h1-bold text-center p-10">Website is developing</h1>;
+  return (
+    <h1 className="h1-bold text-center p-10">
+      🛠️ The website is still under construction 🛠️ We’re currently working hard
+      to bring you a better experience. Some features may not be available at
+      the moment. Thank you for your patience and support. — Imran
+    </h1>
+  );
 };
 
 export default page;
